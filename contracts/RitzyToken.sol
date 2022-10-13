625466b6f06dfc108f480e58a36880270598ec21
Ritzy Dev <RitzyDevBox@gmail.com>
2022-10-12 19:11:56 -0700

    Adding DAO Token

---
+++ contracts/RitzyToken.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract RitzyToken is ERC20Votes, Ownable {
+  uint256 public s_maxSupply = 1000000 ether;
+
+  constructor() ERC20("RitzyToken", "AARTZ") ERC20Permit("RitzyToken") {
+    _mint(msg.sender, s_maxSupply);
+  }
+
+  // The functions below are overrides required by Solidity.
+
+  function _afterTokenTransfer(
+    address from,
+    address to,
+    uint256 amount
+  ) internal override(ERC20Votes) {
+    super._afterTokenTransfer(from, to, amount);
+  }
+
+  function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+    super._mint(to, amount);
+  }
+
+  function _burn(address account, uint256 amount) internal override(ERC20Votes) {
+    super._burn(account, amount);
+  }
+}
---
+++ deploy/06-deploy-ritzy-token.ts
@@ -0,0 +1,38 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import verify from "../helper-functions"
+import { networkConfig, developmentChains } from "../helper-hardhat-config"
+import { ethers } from "hardhat"
+
+const deployGovernanceToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  // @ts-ignore
+  const { getNamedAccounts, deployments, network } = hre
+  const { deploy, log } = deployments
+  const { deployer } = await getNamedAccounts()
+  log("----------------------------------------------------")
+  log("Deploying RitzyToken and waiting for confirmations...")
+  const ritzyToken = await deploy("RitzyToken", {
+    from: deployer,
+    args: [],
+    log: true,
+    // we need to wait if on a live network so we can verify properly
+    waitConfirmations: networkConfig[network.name].blockConfirmations || 1,
+  })
+  log(`RitzyToken at ${ritzyToken.address}`)
+  if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    await verify(ritzyToken.address, [])
+  }
+
+
+  const timeLock = await ethers.getContract("TimeLock")
+  log(`Transfer Ownership to ${timeLock.address}`)
+
+  const ritzyTokenContract = await ethers.getContractAt("RitzyToken", ritzyToken.address)
+  const transferTx = await ritzyTokenContract.transferOwnership(timeLock.address)
+  await transferTx.wait(1)
+  
+  log("Transfered!")
+}
+
+export default deployGovernanceToken
+deployGovernanceToken.tags = ["all", "governor"]
